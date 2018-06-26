bb414af75ba0e28afb9fb5e3022a04948bb03d0a
League <league@WTS8.attlocal.net>
2018-06-26 12:00:17 -0700

    Day 2 of LOAP

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,12 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+		Robot c3p0 = new Robot();
+		c3p0.setSpeed(100);
+		c3p0.penDown();
+		for(int i = 0; i < 4; i++) {
+			c3p0.move(100);
+			c3p0.turn(90);
+		}
 	}
-}
+}	
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,21 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot c3p0 = new Robot();
 		// 5. Set your robot's pen down 
-		
+		c3p0.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		c3p0.setSpeed(500);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for(int i = 0; i < 75; i++) {
 			// 7. Change the pen color to random
-		
+			c3p0.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			c3p0.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+			c3p0.turn(360/7);
 			// 8. Set the pen width to i
-			
+			c3p0.setPenWidth(i);
+		}
 	}
 }
